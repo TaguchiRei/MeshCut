951e66eab0c1605db575f95706752f1fe695aa91
reitaghchi <ren.len2426@gmail.com>
2025-10-23 04:46:11 +0900

    [add]CuttableObjectに当たり判定を単純化するメソッドを追加

--- Assets/Code/Scripts/CuttableObject.cs
+++ Assets/Code/Scripts/CuttableObject.cs
@@ -1,5 +1,10 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Attribute;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class CuttableObject : MonoBehaviour
 {
@@ -7,10 +12,144 @@ public class CuttableObject : MonoBehaviour
     public MeshRenderer MeshRenderer;
     public MeshCollider MeshCollider;
     public Material CutFaceMaterial;
-    
+
+    private List<SphereCollider> _colliders = new();
+
+    [SerializeField] private int _maxSamplingVert = 300;
+    [SerializeField] private int _numberOfCollider;
+
     private void Start()
     {
         var mesh = GetComponent<MeshFilter>().mesh;
         Debug.Log($"オブジェクト{gameObject.name}  頂点数{mesh.vertexCount}");
+    }
+
+
+    [MethodExecutor("当たり判定を単純化する", false)]
+    public void ColliderWeightReduction()
+    {
+        if (MeshCollider.sharedMesh == null) return;
+        int vertCount = MeshCollider.sharedMesh.vertexCount;
+
+        var verts = MeshCollider.sharedMesh.vertices;
+        List<Vector3> clusteringSamples = new();
+        if (verts.Length >= _maxSamplingVert)
+        {
+            for (int i = 0; i < verts.Length; i += vertCount / _maxSamplingVert)
+            {
+                clusteringSamples.Add(verts[i]);
+            }
+        }
+        else
+        {
+            clusteringSamples.AddRange(verts);
+        }
+
+        var centers = ClusteringVerts(clusteringSamples);
+        Vector3 center = Vector3.zero;
+
+        for (int i = 0; i < clusteringSamples.Count; i++)
+        {
+            center += clusteringSamples[i];
+        }
+
+        center /= clusteringSamples.Count;
+
+        foreach (var clusterCenter in centers)
+        {
+            Vector3 colliderCenter = (clusterCenter + center) / 2;
+            float radius = (colliderCenter - clusterCenter).magnitude;
+
+            SphereCollider sphereCollider = gameObject.AddComponent<SphereCollider>();
+            sphereCollider.radius = radius;
+            sphereCollider.center = colliderCenter;
+            _colliders.Add(sphereCollider);
+        }
+    }
+
+    private List<Vector3> ClusteringVerts(List<Vector3> clusteringSample)
+    {
+        List<Vector3> centers = new();
+        List<List<int>> nearVertex = new();
+
+        //クラスタリングの範囲を捜索するための変数
+        float maxX = float.MinValue;
+        float maxY = float.MinValue;
+        float maxZ = float.MinValue;
+        float minX = float.MaxValue;
+        float minY = float.MaxValue;
+        float minZ = float.MaxValue;
+
+        //クラスタリングの範囲の最大値と最小値をサンプルの各座標の最大値と最小値から取得する
+        foreach (var sample in clusteringSample)
+        {
+            if (maxX < sample.x) maxX = sample.x;
+            if (maxY < sample.y) maxY = sample.y;
+            if (maxZ < sample.z) maxZ = sample.z;
+
+            if (minX > sample.x) minX = sample.x;
+            if (minY > sample.y) minY = sample.y;
+            if (minZ > sample.z) minZ = sample.z;
+        }
+
+        for (int i = 0; i < _numberOfCollider; i++)
+        {
+            centers.Add(new Vector3(Random.Range(minX, maxX), Random.Range(minY, maxY), Random.Range(minZ, maxZ)));
+            nearVertex.Add(new List<int>());
+        }
+
+        while (true)
+        {
+            foreach (var nears in nearVertex)
+            {
+                nears.Clear();
+            }
+
+            //重心と頂点を紐づける
+            for (int i = 0; i < clusteringSample.Count; i++)
+            {
+                int mostNear = 0;
+                float distance = float.MaxValue;
+                for (int j = 0; j < centers.Count; j++)
+                {
+                    float newDistance = Vector3.Distance(centers[j], clusteringSample[i]);
+                    if (distance > newDistance)
+                    {
+                        distance = newDistance;
+                        mostNear = j;
+                    }
+                }
+
+                nearVertex[mostNear].Add(i);
+            }
+
+            //重心の位置を頂点の重心に移動する
+            bool changePosition = false;
+            for (int i = 0; i < centers.Count; i++)
+            {
+                if (nearVertex[i].Count == 0) continue;
+
+                var center = centers[i];
+                Vector3 newPosition = Vector3.zero;
+
+                for (int j = 0; j < nearVertex[i].Count; j++)
+                {
+                    newPosition += clusteringSample[nearVertex[i][j]];
+                }
+
+                newPosition /= nearVertex[i].Count;
+
+                if ((newPosition - center).sqrMagnitude > 1e-6f)
+                {
+                    changePosition = true;
+                    centers[i] = newPosition;
+                }
+            }
+
+            if (!changePosition)
+            {
+                return centers;
+            }
+        }
     }
 }
