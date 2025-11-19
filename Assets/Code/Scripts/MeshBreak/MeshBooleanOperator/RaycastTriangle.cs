61b88bb77befb9f33af36e873fbd974d6b41062c
reitaghchi <ren.len2426@gmail.com>
2025-11-19 11:33:52 +0900

    [update]クラス名変更

---
+++ Assets/Code/Scripts/MeshBreak/MeshBooleanOperator/RaycastTriangle.cs
@@ -0,0 +1,14 @@
+using System.Numerics;
+using MeshBreak;
+
+namespace Code.Scripts.MeshBreak.MeshBooleanOperator
+{
+    public static class RaycastTriangle
+    {
+        public static bool CheckIntersectPoint(TriangleData triangle, Vector3 start, Vector3 end, out Vector3 point)
+        {
+            point = Vector3.Zero;
+            return false;
+        }
+    }
+}
