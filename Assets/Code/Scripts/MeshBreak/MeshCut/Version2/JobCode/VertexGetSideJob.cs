24125f8b6c2ae94e9b6f867f3dc5efc1a5bb0dc4
reitaghchi <ren.len2426@gmail.com>
2026-01-08 23:20:47 +0900

    [add]GetSide完成

--- Assets/Code/Scripts/MeshBreak/MeshCut/Version2/JobCode/VertexGetSideJob.cs
+++ Assets/Code/Scripts/MeshBreak/MeshCut/Version2/JobCode/VertexGetSideJob.cs
@@ -4,13 +4,15 @@ using Unity.Mathematics;
 
 public struct VertexGetSideJob : IJobParallelFor
 {
-    [ReadOnly] NativeArray<float3> Vertices;
-    [ReadOnly] NativeArray<NativePlane> Blades;
+    [ReadOnly] public NativeArray<float3> Vertices;
+    [ReadOnly] public NativeArray<NativePlane> Blades;
+    [ReadOnly] public NativeArray<int> BladeIndex;
 
     [WriteOnly] public NativeArray<int> Results;
 
     public void Execute(int index)
     {
-        
+        var blade = Blades[BladeIndex[index]];
+        Results[index] = math.dot(Vertices[index] - blade.Position, blade.Normal) > 0f ? 1 : 0;
     }
 }
